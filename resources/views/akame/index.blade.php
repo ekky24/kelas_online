b600247109b7982302371b2e64c93dda70c5780c
yuanreynaldo0034 <yhuandcool0034>
2019-03-12 13:44:13 +0700

    Update index.blade.php

--- resources/views/akame/index.blade.php
+++ resources/views/akame/index.blade.php
@@ -6,7 +6,7 @@
     <section class="welcome-area">
         <div class="welcome-slides owl-carousel">
             <!-- Single Welcome Slide -->
-            <div class="single-welcome-slide bg-img" style="background-image: url(img/bg-img/online1.jpg);">
+            <div class="single-welcome-slide bg-img" style="background-image: url(img/bg-img/online1.png);">
                 <!-- Welcome Content -->
                 <div class="welcome-content h-100">
                     <div class="container h-100">
@@ -14,7 +14,7 @@
                             <!-- Welcome Text -->
                             <div class="col-12 col-md-9 col-lg-6">
                                 <div class="welcome-text">
-                                    <h2 data-animation="fadeInUp" data-delay="100ms">We Care About <br> Your Hair</h2>
+                                    <h2 data-animation="fadeInUp" data-delay="100ms">We Care About <br> Your Olshop</h2>
                                     <p data-animation="fadeInUp" data-delay="400ms">“Discover your own style. Don't try to repeat what has already been written - have the courage to do your own thing and don't be afraid to do something different.”</p>
                                     <a href="/signup" class="btn akame-btn" data-animation="fadeInUp" data-delay="700ms">Daftar Sekarang</a>
                                 </div>
@@ -23,26 +23,6 @@
                     </div>
                 </div>
             </div>
-
-            <!-- Single Welcome Slide -->
-            <div class="single-welcome-slide bg-img" style="background-image: url(img/bg-img/16.jpg);">
-                <!-- Welcome Content -->
-                <div class="welcome-content h-100">
-                    <div class="container h-100">
-                        <div class="row h-100 align-items-center">
-                            <!-- Welcome Text -->
-                            <div class="col-12 col-md-9 col-lg-6">
-                                <div class="welcome-text">
-                                    <h2 data-animation="fadeInUp" data-delay="100ms">We Care About <br> Your Hair</h2>
-                                    <p data-animation="fadeInUp" data-delay="400ms">“Discover your own style. Don't try to repeat what has already been written - have the courage to do your own thing and don't be afraid to do something different.”</p>
-                                    <a href="/signup" class="btn akame-btn active" data-animation="fadeInUp" data-delay="700ms">Daftar Sekarang</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </section>
     <!-- Welcome Area End -->
 
@@ -103,7 +83,7 @@
                 <div class="col-12 col-sm-6 col-lg-3">
                     <div class="single-service-area mb-80 wow fadeInUp" data-wow-delay="200ms">
                         <img src="img/core-img/s1.png" alt="">
-                        <h5>Coloring</h5>
+                        <h5>DROPSHIPER</h5>
                         <p>Ut enim ad minim veniam, quis trud exercitation...</p>
                     </div>
                 </div>
@@ -112,7 +92,7 @@
                 <div class="col-12 col-sm-6 col-lg-3">
                     <div class="single-service-area mb-80 wow fadeInUp" data-wow-delay="400ms">
                         <img src="img/core-img/s3.png" alt="">
-                        <h5>Haircut</h5>
+                        <h5>FB ADS</h5>
                         <p>Consectetur adipisicing elit, sed doe eiusmod.</p>
                     </div>
                 </div>
@@ -121,7 +101,7 @@
                 <div class="col-12 col-sm-6 col-lg-3">
                     <div class="single-service-area mb-80 wow fadeInUp" data-wow-delay="600ms">
                         <img src="img/core-img/s4.png" alt="">
-                        <h5>Hairstyle</h5>
+                        <h5>ORGANIC MARKETING</h5>
                         <p>Nemo enim ipsam voluptatem quia voluptas</p>
                     </div>
                 </div>
@@ -130,7 +110,7 @@
                 <div class="col-12 col-sm-6 col-lg-3">
                     <div class="single-service-area mb-80 wow fadeInUp" data-wow-delay="800ms">
                         <img src="img/core-img/s3.png" alt="">
-                        <h5>Coloring</h5>
+                        <h5>LANDING PAGE</h5>
                         <p>Ut enim ad minim veniam, quis trud exercitation...</p>
                     </div>
                 </div>
